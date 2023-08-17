20d60513d5c5029d60bcb7fcd408b423a57d9f6e
Unknown1407 <118428191+Unknown1407@users.noreply.github.com>
2023-08-17 17:11:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,10 +5,15 @@ const person = {
   age: 80,
 };
 
-// Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
+// Write your code here, 
+//make sure the name of variables is 
+//exactly same as mentioned in the problem statement
 
 window.players = players;
 window.person = person;
 window.team = team;
 window.team1 = team1;
 window.cap1 = cap1;
+let team = players;
+let team1 = team;
+let cap1 = person;
